c6f18e21a015fbfc8df5d707220f9cea38adfe93
Andrea Bignozzi <andrea.bignozzi@nest2.com>
2012-07-13 16:30:19 +0200

    a

--- lib/generators/usefull_attachment/migration/migration_generator.rb
+++ lib/generators/usefull_attachment/migration/migration_generator.rb
@@ -7,6 +7,16 @@ module UsefullAttachment
     desc "Migration generator for UsefullAttachment gem"
     source_root File.expand_path("../templates", __FILE__)
     
+    def self.next_migration_number(path)
+        unless @prev_migration_nr
+          @prev_migration_nr = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
+        else
+          @prev_migration_nr += 1
+        end
+        @prev_migration_nr.to_s
+    end
+
+    
     def copy_migration
       migration_template "create_migration_template.rb", "db/migrate/create_links.rb"
     end

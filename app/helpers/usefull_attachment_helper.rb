8dff9bbedd97d90cb010967bba51726878448bda
Andrea Bignozzi <andrea.bignozzi@nest2.com>
2012-07-13 19:19:43 +0200

    a

--- app/helpers/usefull_attachment_helper.rb
+++ app/helpers/usefull_attachment_helper.rb
@@ -4,6 +4,8 @@ module UsefullAttachmentHelper
     #ToDo check the relation type and assure is UsefullAttachment::Link
     if object.respond_to?(:links)
       form_for object.links.build, :html => {:multipart => true} do |f|
+        concat(f.text_field :attachmentable_type)
+        concat(f.text_field :attachmentable_id)
         concat(f.file_field :file)
         concat(f.text_field :description)
         concat(f.submit)

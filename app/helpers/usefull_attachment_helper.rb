335adedffd13a27428647def627a7769ecc60472
Andrea Bignozzi <andrea.bignozzi@nest2.com>
2012-07-13 19:24:02 +0200

    a

--- app/helpers/usefull_attachment_helper.rb
+++ app/helpers/usefull_attachment_helper.rb
@@ -1,13 +1,13 @@
 #Add here view and controller helpers
 module UsefullAttachmentHelper
-  def new_attachment_tag(object)
+  def new_attachment_tag(object, description = true)
     #ToDo check the relation type and assure is UsefullAttachment::Link
     if object.respond_to?(:links)
       form_for object.links.build, :html => {:multipart => true} do |f|
-        concat(f.text_field :attachmentable_type)
-        concat(f.text_field :attachmentable_id)
+        concat(f.hidden_field :attachmentable_type)
+        concat(f.hidden_field :attachmentable_id)
         concat(f.file_field :file)
-        concat(f.text_field :description)
+        concat(f.text_field :description) if description
         concat(f.submit)
       end
     end

70b23f5f33a1912ff845c273c429cc05370d0074
Andrea Bignozzi <andrea.bignozzi@nest2.com>
2012-07-13 19:13:52 +0200

    a

--- app/helpers/usefull_attachment_helper.rb
+++ app/helpers/usefull_attachment_helper.rb
@@ -1,4 +1,16 @@
 #Add here view and controller helpers
 module UsefullAttachmentHelper
+  def new_attachment_tag(object)
+    #ToDo check the relation type and assure is UsefullAttachment::Link
+    if object.respond_to?(:links)
+      form_for object.links.build, :html => {:multipart => true} do |f|
+        f.file_tag :file
+        f.text_field :description
+        f.submit
+      end
+    end
+    
+  end
+  
 end
 

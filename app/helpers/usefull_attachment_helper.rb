ca462415c16d0411c6e5d45d122cf45febd76670
Andrea Bignozzi <andrea.bignozzi@nest2.com>
2012-07-16 16:42:08 +0200

    a

--- app/helpers/usefull_attachment_helper.rb
+++ app/helpers/usefull_attachment_helper.rb
@@ -32,8 +32,8 @@ module UsefullAttachmentHelper
     if object.respond_to?(:links) && object.links.present?
       table_for object.links do |t|
         #t.monitor
-        #t.download :url => Proc.new {|object| download_usefull_attachment_link_path(object)} 
-        #t.destroy :url => Proc.new {|object| usefull_attachment_link_path(object)}
+        t.download :url => Proc.new {|object| download_usefull_attachment_link_path(object)} 
+        t.destroy :url => Proc.new {|object| usefull_attachment_link_path(object)}
         t.col :file_file_name
         t.col :description
         t.col :file_file_size if full

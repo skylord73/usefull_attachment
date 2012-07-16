404901a045164e056444b469be79aeb28d880968
Andrea Bignozzi <andrea.bignozzi@nest2.com>
2012-07-16 15:07:50 +0200

    a

--- app/helpers/usefull_attachment_helper.rb
+++ app/helpers/usefull_attachment_helper.rb
@@ -17,7 +17,7 @@ module UsefullAttachmentHelper
   
   def new_avatar_for(object)
     if object.respond_to?(:avatars)
-      form_for object.avatars.build, :html => {:multipart => true} do |f|
+      form_for object.avatars.build, :html => {:multipart => true}, :url => usefull_attachment_links_path do |f|
         concat(f.file_field :file)
         concat(f.submit)
       end

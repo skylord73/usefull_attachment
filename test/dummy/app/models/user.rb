9b58957e0ab8ea1c7874c10ba732514b1f0b6cc2
Andrea Bignozzi <andrea.bignozzi@nest2.com>
2012-07-16 15:02:04 +0200

    sa

--- test/dummy/app/models/user.rb
+++ test/dummy/app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
   has_many :links, :class_name => "UsefullAttachment::Link", :as => :attachmentable
-  has_one :avatar, :class_name => "UsefullAttachment::Avatar", :as => :attachmentable
+  has_many :avatars, :class_name => "UsefullAttachment::Avatar", :as => :attachmentable
 end

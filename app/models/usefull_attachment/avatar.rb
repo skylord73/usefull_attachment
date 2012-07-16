6bafe7551a084a39a693aab307b91a407a0d526a
Andrea Bignozzi <andrea.bignozzi@nest2.com>
2012-07-16 14:58:14 +0200

    a

---
+++ app/models/usefull_attachment/avatar.rb
@@ -0,0 +1,17 @@
+module UsefullAttachment
+  class Avatar < Link
+    
+    
+    private
+    
+    def get_path
+      "/mnt/Webgatec/avatars/:type/:type_id/:base_name.:extension"
+    end
+    
+    def get_url
+      "/images/avatars/:type/:type_id/:base_name.:extension"
+    end
+    
+  end
+  
+end

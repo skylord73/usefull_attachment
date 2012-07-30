7d2547da23cfe41802286721389ef709fd22c86e
Andrea Bignozzi <andrea.bignozzi@nest2.com>
2012-07-30 17:01:21 +0200

    a

--- bin/permission.rb
+++ bin/permission.rb
@@ -2,5 +2,5 @@
 #
 #cambia i permessi alle cartelli utilizzate da paperclip per evitare che file creati da console
 #abbianoi le permissio root e non siano gestibili dal web
-#puts( `sudo chown git:git /mnt/WebGatec/ -R` ) if Rails.env == "development" && `whoami`.strip == "root"
-puts(ARGV[1])
+enviroment = ARGV[0]
+puts( `sudo chown git:git /mnt/WebGatec/ -R` ) if enviroment == "development" && `whoami`.strip == "root"

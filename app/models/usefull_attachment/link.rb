c3c98e248ab3dbacad6e3550ed7e6ecb49b9db5b
Andrea Bignozzi <andrea.bignozzi@nest2.com>
2012-08-28 16:05:07 +0200

    a

--- app/models/usefull_attachment/link.rb
+++ app/models/usefull_attachment/link.rb
@@ -2,7 +2,7 @@
 #
 #Salava i documenti in /mnt/Webgatec/<namespace>/<class>/id/file_name
 #
-#Nel caso sia necessario avere una relazione diversa ovvero 1 attachment -> molti recordo (vedi PhoneQueue) derivare la casse
+#Nel caso sia necessario avere una relazione diversa ovvero 1 attachment -> molti record (vedi PhoneQueue) derivare la casse
 #*  PhoneQueueAttach
 #
 module UsefullAttachment
@@ -12,11 +12,11 @@ module UsefullAttachment
     #Rename file after save if attachmentable provide a rename_attachment method
     before_save do
       #UserSession.log("Admin::Attachment#after_save respond=#{self.attachmentable.inspect}")
-      rename(self.attachmentable.send(:attachment_name, self.file_file_name, self.description)) if self.attachmentable.respond_to?(:attachment_name, true)
-      #UserSession.log("Admin::Attachment#after_save name=#{self.file_file_name}")
+      rename(self.attachmentable.send(:attachment_name, self.link_file_name, self.description)) if self.attachmentable.respond_to?(:attachment_name, true)
+      #UserSession.log("Admin::Attachment#after_save name=#{self.link_file_name}")
     end
     
-    has_attached_file :file,
+    has_attached_file :link,
                       #:path => "/mnt/WebGatec/:type/:type_id/:filename",
                       :path => :get_path,
                       :url => :get_url,
@@ -24,11 +24,11 @@ module UsefullAttachment
                       :styles => {:import => "true"},
                       :processors => [:file_processor]
                       
-    validates_attachment_presence :file
+    #validates_attachment_presence :file
+    validates :link, :attachment_presence => true
     
     belongs_to :attachmentable , :polymorphic => true
-    #has_many :phonequeues, :class_name => "Utility::PhoneQueue", :dependent => :destroy
-    #has_many :attachments, :class_name => "Admin::Attachment", :as => :attachmentable
+    
     
     #Importa il file nel modello dichiarato vedi Spreadsheet::Workbook#to_record
     #*	:model => passa la parent class della tabella però ha sempre la precedenza il nome del foglio di lavoro se è un modello valido
@@ -37,10 +37,10 @@ module UsefullAttachment
     #==Note
     #Passa a to_xls solo i mime-type compatibili
     def to_record(*args)
-      if !file.blank? && self.file_content_type == "application/vnd.ms-excel"
+      if !link.blank? && self.file_content_type == "application/vnd.ms-excel"
         options = args.extract_options!
         options[:model] ||= self.attachmentable_type unless self.attachmentable_type == self.class.name
-        out = self.file.to_record(*(args << options)) 
+        out = self.link.to_record(*(args << options)) 
         #Verifico se il modello è una collection, in caso affermativo lo inserisco automaticamente
         association = find_association(options[:model])
         return self.send(association.to_s + "=", out[options[:model]]) if association
@@ -51,11 +51,11 @@ module UsefullAttachment
     end
     
     def rename(new)
-      unless self.file_file_name.nil?
-        path = self.file.path.split("/")
+      unless self.link_file_name.nil?
+        path = self.link.path.split("/")
         path.pop
-        File.rename(self.file.path, path.push(new).join("/")) if File.exist?(self.file.path)
-        self.file_file_name = new
+        File.rename(self.link.path, path.push(new).join("/")) if File.exist?(self.link.path)
+        self.link_file_name = new
       end
     end
     
@@ -72,7 +72,7 @@ module UsefullAttachment
     
     
     def error_missing?
-      !File.exists?(self.file.path)
+      !File.exists?(self.link.path)
     end
     
     

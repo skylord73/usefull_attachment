e79e28474feb17fca7183d997907b979666f2729
Andrea Bignozzi <andrea.bignozzi@nest2.com>
2012-08-29 15:11:58 +0200

    a

--- app/models/usefull_attachment/link.rb
+++ app/models/usefull_attachment/link.rb
@@ -68,7 +68,7 @@ module UsefullAttachment
     #Rename file after save if attachmentable provide an attachment_name method
     def fill
       to_call_name = "#{self.class.name.underscore.split("/").pop}_name"
-      puts("fill method_name = #{method_name}")
+      puts("fill method_name = #{to_call_name}")
       rename(self.attachmentable.send(to_call_name, self.link_file_name, self.description)) if self.attachmentable.respond_to?(to_call_name, true)
     end
     

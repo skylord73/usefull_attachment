23ccac93ce95ff8ec7c63506a9cb1eff1eba30de
Andrea Bignozzi <andrea.bignozzi@nest2.com>
2012-08-30 14:34:45 +0200

    a

--- app/models/usefull_attachment/link.rb
+++ app/models/usefull_attachment/link.rb
@@ -32,6 +32,7 @@ module UsefullAttachment
     
     belongs_to :attachmentable , :polymorphic => true
     
+    delegate :url, :to => :link
     
     #Importa il file nel modello dichiarato vedi Spreadsheet::Workbook#to_record
     #*	:model => passa la parent class della tabella però ha sempre la precedenza il nome del foglio di lavoro se è un modello valido

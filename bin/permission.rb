b6a9c9882b10468a6614e96a42bbf2052830fc14
ivan.greggio <ivan.greggio@nest2.com>
2013-06-11 17:33:24 +0200

    KioSirio

--- bin/permission.rb
+++ bin/permission.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 #
-#cambia i permessi alle cartelle utilizzate da paperclip per evitare che file creati da console
-#abbiano le permission root e non siano gestibili dal web
+#Cambia i permessi alle cartelle utilizzate da paperclip per evitare che file creati da console
+#abbiano le permission root e non siano gestibili quindi dal web
 enviroment = ARGV[0]
 puts( `sudo chown git:git /mnt/WebGatec/ -R` ) if enviroment == "development" && `whoami`.strip == "root"

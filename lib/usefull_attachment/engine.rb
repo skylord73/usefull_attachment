deabc7267a8bacbbfd3fa37a6b252b9e727a3025
ivan.greggio <ivan.greggio@nest2.com>
2013-06-21 17:55:54 +0200

    KioSirio

--- lib/usefull_attachment/engine.rb
+++ lib/usefull_attachment/engine.rb
@@ -37,10 +37,8 @@ module Paperclip
           begin
 		   # Per controllare se la cartella è vuota verifico il numero di elementi al suo interno e gli tolgo i 2 file "di
 		   # servizio" di Alfresco, ossia "__Forza-Versione.exe" e "__Mostra-Dettagli.exe" presenti in ogni cartella esposta
-		   # da Alfresco.
-		   puts "#{ Dir.entries(File.dirname(path)).size - 2 }"
-		   puts "#{ Dir.entries(File.dirname(path)).inspect }"
-		   while(Dir.entries(File.dirname(path)).size - 2 == 0)
+		   # da Alfresco. Gli tolgo inoltre le cartelle/riferimenti "." e "..".
+		   while(Dir.entries(File.dirname(path)).size - 2 - 2 == 0)
 			 FileUtils.rmdir(path)
 			 path = File.dirname(path)
 		   end

7fd3835ed21d7098d5e181be0a298ca8ab807a54
ivan.greggio <ivan.greggio@nest2.com>
2013-06-21 16:49:38 +0200

    KioSirio

--- lib/usefull_attachment/engine.rb
+++ lib/usefull_attachment/engine.rb
@@ -17,3 +17,40 @@ module UsefullAttachment
 end
 
 #Add ere require to specific file or gem used
+
+require "paperclip"
+module Paperclip
+  module Storage
+    module Filesystem
+
+      # Patch alla cancellazione di Paperclip che tenta di cancellare tutto quello che può finchè non fallisce la cancellazione
+      # a livello di sistema operativo, tale modalità di cancellazione però crea problemi con le share su ALfresco montate
+      # tramite CIFS.
+      def flush_deletes #:nodoc:
+        puts "STO ESEGUENDO IL METODO MIO !!!"
+        @queued_for_delete.each do |path|
+          begin
+            log("deleting #{path}")
+            FileUtils.rm(path) if File.exist?(path)
+          rescue Errno::ENOENT => e
+            # ignore file-not-found, let everything else pass
+          end
+          begin
+            while(true)
+              path = File.dirname(path)
+              FileUtils.rmdir(path)
+              break if File.exists?(path) # Ruby 1.9.2 does not raise if the removal failed.
+            end
+          rescue Errno::EEXIST, Errno::ENOTEMPTY, Errno::ENOENT, Errno::EINVAL, Errno::ENOTDIR, Errno::EACCES
+            # Stop trying to remove parent directories
+          rescue SystemCallError => e
+            log("There was an unexpected error while deleting directories: #{e.class}")
+            # Ignore it
+          end
+        end
+        @queued_for_delete = []
+      end
+	
+    end
+  end
+end

954566abaca82ad50eb102ecc61d5c6c08205d9a
ivan.greggio <ivan.greggio@nest2.com>
2013-06-24 12:32:29 +0200

    KioSirio

--- lib/usefull_attachment/exceptions.rb
+++ lib/usefull_attachment/exceptions.rb
@@ -1,10 +1,21 @@
 #Localize messages in it.CustomError.MyError
 module UsefullAttachment
-  module Links
+
+  class CustomError < StandardError
+    def initialize(*args)
+        @options = args.extract_options!
+        super
+    end
       
+    def message
+      @options.merge!({:default => "Error : #{@options.inspect}"})
+      I18n.t("#{self.class.name.gsub(/::/,'.')}", @options )
+    end
+  end 
+
+  module Links
     class FileDuplicate < ::CustomError ; end
-  
     #class MyError < CustomError; end
-	
   end
+
 end
